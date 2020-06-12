bfcbfd924435354ce395f41a7bc86cadf5923a71
Marcin Robak <mr.robak@gmail.com>
2020-06-12 15:54:11 +0200

    add home page and navigations

--- App.js
+++ App.js
@@ -12,12 +12,15 @@ import {
   FlatList,
   StyleSheet,
 } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import FlexBox from "./components/FlexBox";
 import GameScreen from "./components/GameScreen";
 import ShareExample from "./components/ ShareExample";
+import HomeScreen from "./components/HomeScreen";
 
 const styles = StyleSheet.create({
   dino: { fontSize: 20, fontWeight: "bold", color: "rebeccapurple" },
@@ -27,16 +30,38 @@ const styles = StyleSheet.create({
   screen: { marginBottom: 20 },
 });
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.screen}>
-      <GameScreen />
-      {/* <View style={{ marginHorizontal: 10, marginVertical: 80 }}> */}
-      {/* <FlexBox /> */}
-      {/* <Gallery /> */}
-      {/* <Text style={styles.dino}>Dinosaurs are cool</Text> */}
-      {/* <Footer /> */}
-      {/* </View> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome!" }}
+        />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    // <View style={styles.screen}>
+    //  {/* <GameScreen /> */}
+    //  {/* <View style={{ marginHorizontal: 10, marginVertical: 80 }}> */}
+    //  {/* <FlexBox /> */}
+    //  {/* <Gallery /> */}
+    //  {/* <Text style={styles.dino}>Dinosaurs are cool</Text> */}
+    //  {/* <Footer /> */}
+    //  {/* </View> */}
+    // </View>
   );
 }
